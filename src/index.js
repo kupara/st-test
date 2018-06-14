e65bb837e5fc2e91920b5721895f28d7dfbd3c7b
Edwin Kupara <kewekesa@gmail.com>
2018-06-14 19:07:32 +0300

    Project set up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+render(<App />, rootElement);
+
+// console.log('Hello World');
